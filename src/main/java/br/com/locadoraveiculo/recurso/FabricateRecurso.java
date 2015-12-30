f40bdd59063abf7603eda4227f34e6811be7c573
michel <michel@MAM-PCL>
2015-12-30 17:58:47 -0200

    Serviço Fabricante está completo.

--- src/main/java/br/com/locadoraveiculo/recurso/FabricateRecurso.java
+++ src/main/java/br/com/locadoraveiculo/recurso/FabricateRecurso.java
@@ -5,16 +5,20 @@
  */
 package br.com.locadoraveiculo.recurso;
 
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
 import com.algaworks.curso.jpa2.modelo.Fabricante;
 import com.algaworks.curso.jpa2.service.CadastroFabricanteService;
 import com.algaworks.curso.jpa2.service.NegocioException;
 import com.google.gson.Gson;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,12 +31,15 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class FabricateRecurso {
-    
+
     @Inject
     private CadastroFabricanteService cadastroFabricanteService;
-    
+
+    @Inject
+    private FabricanteDAO fabricanteDAO;
+
     private Fabricante fabricante;
-    
+
     @Path("inserirFabricante")
     @POST
     public Response salvar(String conteudo) {
@@ -53,10 +60,41 @@ public class FabricateRecurso {
                     .build();
         } catch (NegocioException ex) {
             //return Response.notModified(ex.getMessage());
-            System.out.println("erro salvar Fabricante "+ex.getMessage());
+            System.out.println("erro salvar Fabricante " + ex.getMessage());
         }
 
         return null;
     }
-    
+
+    @Path("todos")
+    @GET
+    public List<Fabricante> buscaTudo() {
+        List<Fabricante> listaFabricantes = new ArrayList<>();
+        listaFabricantes = fabricanteDAO.buscarTodos();
+        return listaFabricantes;
+    }
+
+    @Path("todos/{palavra}")
+    @GET
+    public List<Fabricante> buscarEspecifico(@PathParam("palavra") String palavra) {
+        System.out.println("palavra "+palavra);
+        List<Fabricante> listaFabricantes = new ArrayList<>();
+        listaFabricantes = fabricanteDAO.buscarEspecifico(palavra);
+        return listaFabricantes;
+    }
+
+    @Path("remove/{id}")
+    @DELETE
+    public Response remove(@PathParam("id") Long id) {
+        try {
+            fabricanteDAO.excluirFabricante(id);
+        } catch (Exception ex) {
+            System.out.println("erro ao excluir Fabricante " + ex.getMessage());
+        }
+        return Response.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+                .build();
+    }
+
 }
--- src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -6,23 +6,40 @@
 package com.algaworks.curso.jpa2.dao;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 import java.io.Serializable;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
 
 /**
  *
  * @author Michel A. Medeiros
  */
-public class FabricanteDAO implements Serializable{
+public class FabricanteDAO implements Serializable {
 
     @Inject
     private EntityManager em;
-    
-    public Fabricante salvar(Fabricante fabricante){
+
+    public Fabricante salvar(Fabricante fabricante) {
         fabricante = em.merge(fabricante);
         return fabricante;
     }
-    
+
+    public List<Fabricante> buscarTodos() {
+        return em.createQuery("Select f From Fabricante f").getResultList();
+    }
+
+    public List<Fabricante> buscarEspecifico(String palavra) {
+        return em.createQuery("Select f From Fabricante f "
+                + "WHERE f.nome LIKE '%"+palavra+"%' ").getResultList();
+    }
+
+    @Transactional
+    public void excluirFabricante(Long id) {
+        Fabricante f = em.find(Fabricante.class, id);
+        em.remove(f);
+        em.flush();
+    }
+
 }
